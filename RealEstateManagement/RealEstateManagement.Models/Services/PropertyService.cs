5b107247f3f60a9cf62bfff8691f8561290203e7
Hasaan Gilani <gilani.4@gmail.com>
2013-02-10 22:04:18 -0500

    property service

---
+++ RealEstateManagement/RealEstateManagement.Models/Services/PropertyService.cs
@@ -0,0 +1,14 @@
+﻿using RealEstateManagement.Core.DAL;
+using RealEstateManagement.Core.Models;
+
+namespace RealManagement.Services.Properties
+{
+	public class PropertyService
+	{
+		public void AddNewProperty(Property property, RealEstateManagementContext dbContext)
+		{
+			dbContext.Properties.Add(property);
+			dbContext.SaveChanges();
+		}
+	}
+}
---
+++ RealEstateManagement/RealEstateManagement.Tests/Services/PropertyServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RealEstateManagement.Tests.Services
+{
+	[TestClass]
+	public class PropertyServiceTests
+	{
+		[TestMethod]
+		public void ToAddAProperty_ShouldCallSaveOnce()
+		{
+			//var dbContextMock = new Mock<RealEstateManagementContext>();
+			
+			//Cant use this because its not overrideable or cant be made virtual
+			//dbContextMock.Setup(x=>x.Properties.Add(It.IsAny<Property>())).Verifiable();
+			//var propertyMock = new Mock<Property>();
+
+			//var propertyService = new PropertyService();
+
+			
+
+			//propertyService.AddNewProperty(propertyMock.Object, dbContextMock.Object);
+
+			//dbContextMock.Verify(x=>x.Properties.Add(It.IsAny<Property>()),Times.Once());
+			//dbContextMock.Verify(x=>x.SaveChanges(),Times.Once());
+			
+			//propertyServiceMock.Setup(x=>x.AddProperty(It.IsAny<Property>(), It.IsAny<DbContext>()))
+		}
+	}
+}
